864d84b85fc4a06a97cd4fa13b01d853f09943b1
Romain <mromain911@gmail.com>
2023-01-17 17:45:21 +0100

    UI, script interaction dialogue
    l'ui pour les dialogues et pour la bulle d'intérection avec la touche indiqué.
    Script pour intéragir sur un pnj

---
+++ Assets/Narrative/Script/DialogueTalkZone.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTalkZone : MonoBehaviour
+{
+    [SerializeField] private GameObject speechBubble;
+    [SerializeField] private KeyCode talkKey = KeyCode.E;
+    [SerializeField] private Text keyInputText;
+
+    private void Awake()
+    {
+        speechBubble.SetActive(false);
+        keyInputText.text = talkKey.ToString();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(talkKey) && speechBubble.activeSelf)
+        {
+            // TODO: Start Dialogue.
+            Debug.Log("Start Dialouge");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            speechBubble.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            speechBubble.SetActive(false);
+        }
+    }
+}
