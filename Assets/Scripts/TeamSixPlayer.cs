0e8f8a21e6c79e61c5960fc6f985dbdf1e555a87
AkuroP <thomas.kang2002@gmail.com>
2023-01-23 13:39:53 +0100

    Mini Scene interaction + Auto Move

--- Assets/Scripts/TeamSixPlayer.cs
+++ Assets/Scripts/TeamSixPlayer.cs
@@ -6,14 +6,36 @@ public class TeamSixPlayer : MonoBehaviour
 {
     [SerializeField]private float playerSpeed;
     [SerializeField]private bool playerIsMoving;
+
+    public bool PlayerIsMoving
+    {
+        get{return this.playerController;}
+        set{playerIsMoving = value;}
+    }
+
+
     private CharacterController playerController;
+    public CharacterController PlayerController
+    {
+        get{return this.playerController;}
+        set{playerController = value;}
+    }
 
     private Coroutine moveCoroutine;
+
+    [SerializeField]private bool canMove = true;
+    
+    public bool CanMove
+    {
+        get{return canMove;}
+        set{this.canMove = value;}
+    }
     
     // Start is called before the first frame update
     private void Start()
     {
         playerController = this.GetComponent<CharacterController>();
+        canMove = true;
     }
 
     // Update is called once per frame
@@ -26,20 +48,30 @@ public class TeamSixPlayer : MonoBehaviour
     //Start Moving player, direction : 1 = Right ; 2 = Left.
     public void PlayerStartMoving(int dir)
     {
+        if(!canMove)return;
         //reset movement if already moving
         if(playerIsMoving)StopCoroutine(moveCoroutine);
 
         //set settings for moving
         playerIsMoving = true;
         Vector2 PlayerDir = new Vector2(dir, 0f);
+        FlipPlayer(dir);
         moveCoroutine = StartCoroutine(Move(PlayerDir));
+    }
+
+    public void FlipPlayer(float dir)
+    {
+        if(dir > 0)this.GetComponent<SpriteRenderer>().flipX = false;
+        else if(dir < 0)this.GetComponent<SpriteRenderer>().flipX = true;
     }
 
     //Stop Moving player
     public void PlayerStopMoving()
     {
+        if(!canMove)return;
         //reset settings for moving
         playerIsMoving = false;
+        
         StopCoroutine(moveCoroutine);
     }
 
@@ -51,6 +83,11 @@ public class TeamSixPlayer : MonoBehaviour
             playerController.Move(dir * playerSpeed * Time.deltaTime);
             yield return null;   
         }
+    }
+
+    public void SetMovement(bool movable)
+    {
+        this.canMove = movable;
     }
     #endregion
 }
