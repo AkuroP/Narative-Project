be657216ec28dca30a918d977ad27edc2f000a8b
AkuroP <thomas.kang2002@gmail.com>
2023-01-27 15:43:41 +0100

    new anim integrated

--- Assets/Team06/Scripts/GoToNextZone.cs
+++ Assets/Team06/Scripts/GoToNextZone.cs
@@ -12,6 +12,9 @@ namespace Team06
         public Transform tpPoint;
 
         public TeamSixPlayer player;
+
+        public Vector3 newSize = new Vector3(.3f, .3f, .3f);
+        public float newFOV = 25f;
         
 
         private void OnTriggerEnter2D(Collider2D collider)
@@ -32,6 +35,8 @@ namespace Team06
             yield return new WaitForSeconds(.45f);
             player.transform.position = tpPoint.position;
             confiner.m_BoundingShape2D = nextConfiner;
+            player.transform.localScale = newSize;
+            player.confiner.GetComponent<Cinemachine.CinemachineVirtualCamera>().m_Lens.FieldOfView = newFOV / 2;
             player.CanMove = true;
             yield return new WaitForSeconds(.45f);
         }
